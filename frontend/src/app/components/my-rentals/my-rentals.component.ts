f9a803f04c0ac317e688af01e0f6dc4e73912fda
sameer pathan <sameerpathan40694@gmail.com>
2026-02-14 11:59:52 +0530

    Update my-rentals.component.ts

--- frontend/src/app/components/my-rentals/my-rentals.component.ts
+++ frontend/src/app/components/my-rentals/my-rentals.component.ts
@@ -42,7 +42,7 @@ export class MyRentalsComponent implements OnInit {
 
     if (this.activeSection === 'wishlist') {
       this.http
-        .get<any[]>(`renteasy-production.up.railway.app/wishlist/${this.userName}`)
+        .get<any[]>(`https://renteasy-production.up.railway.app/wishlist/${this.userName}`)
         .subscribe({
           next: res => {
             this.wishlist = res;
@@ -57,7 +57,7 @@ export class MyRentalsComponent implements OnInit {
 
     if (this.activeSection === 'applications') {
   this.http
-    .get<any[]>(`renteasy-production.up.railway.app/application/tenant/${this.userName}`)
+    .get<any[]>(`https://renteasy-production.up.railway.app/application/tenant/${this.userName}`)
     .subscribe({
       next: res => {
   this.applications = res
@@ -112,7 +112,7 @@ export class MyRentalsComponent implements OnInit {
   }
 
   this.http
-    .delete(`renteasy-production.up.railway.app/application/delete/${appId}`)
+    .delete(`https://renteasy-production.up.railway.app/application/delete/${appId}`)
     .subscribe({
       next: () => {
         //  remove from UI instantly
