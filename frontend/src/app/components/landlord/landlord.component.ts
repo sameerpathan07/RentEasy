3aac86a2ad24dc77f00ca16fc4c36e3f660e8ddd
sameer pathan <sameerpathan40694@gmail.com>
2026-02-14 11:58:49 +0530

    Update landlord.component.ts

--- frontend/src/app/components/landlord/landlord.component.ts
+++ frontend/src/app/components/landlord/landlord.component.ts
@@ -49,7 +49,7 @@ openProperty(id: number) {
     this.loading = true;
 
     this.http
-      .get<any[]>(`renteasy-production.up.railway.app/application/user/${this.userName}`)
+      .get<any[]>(`https://renteasy-production.up.railway.app/application/user/${this.userName}`)
       .subscribe({
         next: res => {
           this.applications = res;
@@ -67,7 +67,7 @@ openProperty(id: number) {
  updateStatus(appId: number, status: string) {
   this.http
     .put(
-      `renteasy-production.up.railway.app/application/update/${appId}?status=${status}`,
+      `https://renteasy-production.up.railway.app/application/update/${appId}?status=${status}`,
       {}
     )
     .subscribe({
@@ -88,7 +88,7 @@ deleteApplication(appId: number) {
   }
 
   this.http
-    .delete(`renteasy-production.up.railway.app/application/delete/${appId}`)
+    .delete(`https://renteasy-production.up.railway.app/application/delete/${appId}`)
     .subscribe({
       next: () => {
         // 🔥 remove from UI instantly
@@ -121,7 +121,7 @@ switchTab(tab: 'properties' | 'applications') {
   this.loadingProperties = true;
 
   this.http
-    .get<any[]>(`renteasy-production.up.railway.app/image/my/${this.userName}`)
+    .get<any[]>(`https://renteasy-production.up.railway.app/image/my/${this.userName}`)
     .subscribe({
       next: res => {
         this.properties = res;
