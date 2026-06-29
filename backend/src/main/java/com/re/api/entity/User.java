c89fad2b85ade6a2892ffa73932b0ee1cd51b970
sameer pathan <sameerpathan40694@gmail.com>
2026-06-29 17:21:23 +0530

    Update User.java

--- backend/src/main/java/com/re/api/entity/User.java
+++ backend/src/main/java/com/re/api/entity/User.java
@@ -8,6 +8,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
