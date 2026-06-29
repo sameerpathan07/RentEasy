8b3c92152360dbe1854c53fc9f0c413746f301bb
sameer pathan <sameerpathan40694@gmail.com>
2026-06-29 15:05:13 +0530

    Update user.service.ts

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
-  private baseUrl = 'https://renteasy-production.up.railway.app/user';
+  private baseUrl = 'https://renteasy-fix3.onrender.com.app/user';
 
   constructor(private http: HttpClient) {}
 
